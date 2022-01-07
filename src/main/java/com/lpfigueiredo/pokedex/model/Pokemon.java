41b018beba239cd7eb4e734373950c3522e0985f
Leonardo Paulo Figueiredo <leonardo.figueiredo@tecnospeed.local>
2022-01-07 11:58:52 -0300

    feat: configure MongoDB data

--- src/main/java/com/lpfigueiredo/pokedex/model/Pokemon.java
+++ src/main/java/com/lpfigueiredo/pokedex/model/Pokemon.java
@@ -11,11 +11,18 @@ public class Pokemon {
     @Id
     private String id;
 
-
     private String nome;
     private String categoria;
     private String habilidade;
     private Double peso;
+
+    public Pokemon(String id, String nome, String categoria, String habilidade, Double peso) {
+        this.id = id;
+        this.nome = nome;
+        this.categoria = categoria;
+        this.habilidade = habilidade;
+        this.peso = peso;
+    }
 
     public String getId() {
         return id;
